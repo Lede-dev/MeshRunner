353517960ccb590fe0b942fe71a0b961abc08444
Lede-dev <lede.7716@gmail.com>
2024-01-26 10:47:26 +0900

    Feat: Add ingame game mode

---
+++ Source/MeshRunner/Public/GameMode/MeshRunnerGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeshRunnerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MESHRUNNER_API AMeshRunnerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
